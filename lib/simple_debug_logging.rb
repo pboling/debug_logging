70e880fdfc27aa1d28a16f7ff6db6a0ecf4b7444
Peter Boling <peter.boling@gmail.com>
2017-09-26 01:54:40 -0700

    Actual working example for how to use the bauble

--- lib/simple_debug_logging.rb
+++ lib/simple_debug_logging.rb
@@ -1,11 +1,42 @@
 # Simpler version of what the sibling DebugLogging library does.  Included as a bauble.
 #
-# Automatically log Class.method(arguments) as they are called at runtime!
+############# THIS IS A BAUBLE
+############# FOR EXAMINGING SEVERAL OF THE WONDERS OF RUBY
+############# TO ACCOMPLISH SOMETHING PRACTICAL
+############# For a more robust implementation use the gem debug_logging itself, 
+#############   which makes use of these same principles.
+#
+# Automatically log Class.method(arguments) as they are called at runtime (instance or singleton)!
+#
+#   Create a file `unobtrusively_logged.rb` with:
+#
+#     require 'simple_debug_logging'
+#     class UnobtrusivelyLogged
+#       def initialize(**args); end
+#       def an_instance_method(*args); end
+#       def self.a_class_method(*args); self; end
+#       include SimpleDebugLogging.new(i_methods: %i( initialzie an_instance_method ))
+#       logged :a_class_method
+#     end
+#
+#   Make sure you have the latest debug_logging gem (>= 1.0.10) installed for this to work
+#
+#   In an irb session:
+#     >> require_relative 'unobtrusively_logged'
+#     => true
+#     >> UnobtrusivelyLogged.a_class_method.new.an_instance_method
+#     UnobtrusivelyLogged.a_class_method() ~70156299674920@1506415414~
+#     UnobtrusivelyLogged.a_class_method ~689933733604307372~ complete in 8.000002708286047e-06s ~70156299674920@1506415414~
+#     UnobtrusivelyLogged#an_instance_method() ~70156299673760@1506415414~
+#     UnobtrusivelyLogged#an_instance_method ~689933733604307372~ complete in 2.00001522898674e-06s ~70156299673760@1506415414~
 #
 # NOTE: For a more advanced version see the debug_logging gem
 # NOTE: The manner this is made to work for class methods is totally different than the way this is made to work for instance methods.
 # NOTE: The instance method manner of logging works on Ruby 2.0+
 # NOTE: The class method manner of logging works on Ruby 2.1+
+
+require 'benchmark'
+
 class SimpleDebugLogging < Module
   def initialize(i_methods: nil)
     @instance_methods_to_log = Array(i_methods) if i_methods
